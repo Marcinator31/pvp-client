4fa6716af37d36257b7292cc66a02fcede4ae6fc
Marcinator31 <marcinator31@gmail.com>
2026-06-21 14:10:53 +0200

    Add files via upload

---
+++ src/client/java/com/example/pvpclient/module/modules/LowFireModule.java
@@ -0,0 +1,10 @@
+package com.example.pvpclient.module.modules;
+
+import com.example.pvpclient.module.ResourcePackModule;
+
+/** Aktiviert das gebuendelte "Low Fire"-Pack (niedrigere Flammen beim Brennen). */
+public class LowFireModule extends ResourcePackModule {
+    public LowFireModule() {
+        super("Low Fire", Category.MISC, "lowfire");
+    }
+}
---
+++ src/client/java/com/example/pvpclient/module/modules/NoPumpkinBlurModule.java
@@ -0,0 +1,10 @@
+package com.example.pvpclient.module.modules;
+
+import com.example.pvpclient.module.ResourcePackModule;
+
+/** Aktiviert das gebuendelte "No Pumpkin Blur"-Pack (klare Sicht mit Kuerbis). */
+public class NoPumpkinBlurModule extends ResourcePackModule {
+    public NoPumpkinBlurModule() {
+        super("No Pumpkin Blur", Category.MISC, "nopumpkinblur");
+    }
+}
