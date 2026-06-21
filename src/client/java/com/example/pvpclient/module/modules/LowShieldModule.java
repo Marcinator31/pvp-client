0a60b3ae8ffa42ab6278731a677e6d61b87657ab
Marcinator31 <marcinator31@gmail.com>
2026-06-21 14:19:18 +0200

    Add files via upload

---
+++ src/client/java/com/example/pvpclient/module/modules/LowShieldModule.java
@@ -0,0 +1,10 @@
+package com.example.pvpclient.module.modules;
+
+import com.example.pvpclient.module.ResourcePackModule;
+
+/** Aktiviert das gebuendelte "Low Shield"-Pack (Schild weniger im Sichtfeld). */
+public class LowShieldModule extends ResourcePackModule {
+    public LowShieldModule() {
+        super("Low Shield", Category.MISC, "lowshield");
+    }
+}
