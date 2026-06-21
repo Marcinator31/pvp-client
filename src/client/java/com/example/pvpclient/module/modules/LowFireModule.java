9b63825a81f91da986b63db20c5f34189f530710
Marcinator31 <marcinator31@gmail.com>
2026-06-21 18:53:36 +0200

    Add files via upload

---
+++ src/client/java/com/example/pvpclient/module/modules/LowFireModule.java
@@ -0,0 +1,10 @@
+package com.example.pvpclient.module.modules;
+
+import com.example.pvpclient.module.Module;
+
+/** Low Fire -- niedrigere Flammen beim Brennen. Arbeit macht LowFireMixin. */
+public class LowFireModule extends Module {
+    public LowFireModule() {
+        super("Low Fire", Category.MISC);
+    }
+}
