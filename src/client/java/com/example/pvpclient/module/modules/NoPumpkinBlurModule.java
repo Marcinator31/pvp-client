bd1793a4a474b0569c385a89580c705091518974
Marcinator31 <marcinator31@gmail.com>
2026-06-21 23:36:44 +0200

    Add files via upload

---
+++ src/client/java/com/example/pvpclient/module/modules/NoPumpkinBlurModule.java
@@ -0,0 +1,13 @@
+package com.example.pvpclient.module.modules;
+
+import com.example.pvpclient.module.Module;
+
+/**
+ * No Pumpkin Blur -- klare Sicht trotz Kuerbis auf dem Kopf.
+ * Die Arbeit macht der NoPumpkinBlurMixin (Vorbild: BactroMod).
+ */
+public class NoPumpkinBlurModule extends Module {
+    public NoPumpkinBlurModule() {
+        super("No Pumpkin Blur", Category.MISC);
+    }
+}
